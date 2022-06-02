5e8bb813725d5f2966e812e11534f935a138f2db
Camilo Morón <atontaurus@gmail.com>
2022-06-02 13:07:03 -0400

    Crear clase, función incremento, crear objeto, añadir puerta y mostrar número de puertas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas();
+        System.out.println(miCoche.puertas);
     }
+    class Coche {
+        public int puertas = 4;
+
+        public void aumentarPuertas() {
+            this.puertas++;
+        }
+    }
+
+
 }
